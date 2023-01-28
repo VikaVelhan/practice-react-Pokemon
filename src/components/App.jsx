3ec47277a12076c3b9db7ad4172e632408294a4a
VikaVelhan <velganvika@gmail.com>
2023-01-28 21:26:17 +0200

    componentDidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    pokemon: null,
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(res => res.json())
+      .then(pokemon => this.setState({ pokemon }))
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          maxWidth: 1170,
+          margin: '0 auto',
+          padding: 20,
+        }}
+      >
+        {this.state.loading && <h1> загружається...</h1>}
+        {this.state.pokemon && <div> {this.state.pokemon.name}</div>}
+      </div>
+    );
+  }
+}
